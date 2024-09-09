0b67732ced3d7cd40f92cfcce5f537bac95a1550
arildo <arildohp@gmail.com>
2024-09-09 16:39:01 -0300

    finalizando  aula

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
-const sourcemaps = require("gulp-sourcemaps");
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate');
+const imagemin = require('gulp-imagemin');
+
+function comprimeImagens() {
+   return gulp.src('./source/images/*')
+       .pipe(imagemin())
+       .pipe(gulp.dest('./build/images'))
+}
+
+function comprimeJavaScript() {
+   return gulp.src('./source/scripts/*.js')
+       .pipe(uglify())
+       .pipe(obfuscate())
+       .pipe(gulp.dest('./build/scripts'))
+}
 
 function compilaSass() {
     return gulp.src('./source/styles/main.scss')
@@ -14,6 +30,8 @@ function compilaSass() {
 
 
 exports.sass = compilaSass;
-exports.watch = function() {
-        gulp.watch('./source/styles/*.scss',  { ignoreInitial: false}, gulp.series(compilaSass))
+exports.default = function() {
+   gulp.watch('./source/styles/*.scss',  { ignoreInitial: false}, gulp.series(compilaSass));
+   gulp.watch('./source/scripts/*.js',  { ignoreInitial: false}, gulp.series(comprimeJavaScript));
+   gulp.watch('./source/images/*',  { ignoreInitial: false}, gulp.series(comprimeImagens));
 }
